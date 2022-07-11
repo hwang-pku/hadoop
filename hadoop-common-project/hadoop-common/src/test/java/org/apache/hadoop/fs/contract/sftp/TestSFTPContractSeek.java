6e24f2af63b42f6f80530ced1aff49c07072b10f
Tony Wang <tony.wanghao@stu.pku.edu.cn>
2022-07-12 01:51:22 +0800

    rewrite TestSFTPContractSeek

--- hadoop-common-project/hadoop-common/src/test/java/org/apache/hadoop/fs/contract/sftp/TestSFTPContractSeek.java
+++ hadoop-common-project/hadoop-common/src/test/java/org/apache/hadoop/fs/contract/sftp/TestSFTPContractSeek.java
@@ -18,11 +18,44 @@
 
 package org.apache.hadoop.fs.contract.sftp;
 
+import java.util.Arrays;
+import java.util.Collection;
+
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.contract.AbstractContractSeekTest;
 import org.apache.hadoop.fs.contract.AbstractFSContract;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+import org.junit.runners.Parameterized.Parameter;
 
+@RunWith(Parameterized.class)
 public class TestSFTPContractSeek extends AbstractContractSeekTest {
+  @Parameter(0)
+  public String hadoopKerberosKeytabLoginAutorenewalEnabled;
+
+  @Parameter(1)
+  public String hadoopSecurityAuthToLocalMechanism;
+
+  @Before
+  public void setUp() {
+      Configuration.MYHACK.clear();
+      Configuration.MYHACK.put("hadoop.kerberos.keytab.login.autorenewal.enabled",
+          this.hadoopKerberosKeytabLoginAutorenewalEnabled);
+      Configuration.MYHACK.put("hadoop.security.auth_to_local.mechanism",
+          this.hadoopSecurityAuthToLocalMechanism);
+  }
+
+  @Parameters(name = "hadoop.kerberos.keytab.login.autorenewal.enabled={0},hadoop.security.auth_to_local.mechanism={1}")
+  public static Collection params() {
+    return Arrays.asList(new Object[][]{
+        {"true", "hadoop"},
+        {"true", "MIT"},
+        {"false", "hadoop"},
+        {"false", "MIT"}
+    });
+  }
 
   @Override
   protected AbstractFSContract createContract(Configuration conf) {
