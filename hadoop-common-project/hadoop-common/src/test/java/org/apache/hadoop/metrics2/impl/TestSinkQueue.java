4f9194c78dc76ad387f23f36075bb908bbac397c
Monil Narang <monil.narang@mindtickle.com>
2022-08-12 19:30:42 +0530

    Parameterizing test 6 :- testClear

--- hadoop-common-project/hadoop-common/src/test/java/org/apache/hadoop/metrics2/impl/TestSinkQueue.java
+++ hadoop-common-project/hadoop-common/src/test/java/org/apache/hadoop/metrics2/impl/TestSinkQueue.java
@@ -211,6 +211,7 @@ public class TestSinkQueue {
   /**
    * Test the clear method
    */
+  // Class #1 PUT #6
   @Test public void testClear() {
     final SinkQueue<Integer> q = new SinkQueue<Integer>(128);
     for (int i = 0; i < q.capacity() + 97; ++i) {
